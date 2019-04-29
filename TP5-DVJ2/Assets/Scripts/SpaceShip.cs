d998f7ca101be66e979213ca8d53c6d9e2ecc8ba
fedepvg <federicopvg@gmail.com>
2019-04-28 21:47:16 -0300

    movement and physics improved

--- TP5-DVJ2/Assets/Scripts/SpaceShip.cs
+++ TP5-DVJ2/Assets/Scripts/SpaceShip.cs
@@ -8,38 +8,59 @@ public class SpaceShip : MonoBehaviour
     [SerializeField]
     private float SpeedZ;
     [SerializeField]
-    private float XRotationRate;
+    private float PitchRate;
     [SerializeField]
-    private float ZRotationRate;
-    float InputX;
-    float InputZ;
+    private float RollRate;
+    [SerializeField]
+    private float YawRate;
+    float InputPitch;
+    float InputRoll;
+    float InputYaw;
+    float InputAcc;
+    float BaseLinearForce;
     Vector2 MoveInput;
-    Vector3 MoveDir=Vector3.zero;
+    Vector3 MoveDir = Vector3.zero;
     void Awake()
     {
         rigi = GetComponentInParent<Rigidbody>();
-        SpeedZ = 20f;
-        XRotationRate = 70f;
-        ZRotationRate = -70f;
+        SpeedZ = 7000f;
+        PitchRate = 700;
+        RollRate = -700;
+        YawRate = 700;
+        BaseLinearForce = 300f;
+        Physics.gravity = new Vector3(0f,-91.8f, 0f);
+    }
+
+    private void Start()
+    {
+        rigi.AddRelativeForce(new Vector3(0, 0, BaseLinearForce * SpeedZ * InputAcc * Time.deltaTime), ForceMode.Impulse);
     }
 
     private void FixedUpdate()
     {
-        GetInput(ref InputX,ref InputZ);
-        
+        GetInput(ref InputPitch, ref InputRoll, ref InputYaw, ref InputAcc);
 
-        rigi.AddRelativeTorque(InputX * XRotationRate * Time.deltaTime, 0f, InputZ * ZRotationRate * Time.deltaTime, ForceMode.Force);
 
-        rigi.AddRelativeForce(new Vector3(0, 0, 10 * SpeedZ));
+        rigi.AddRelativeTorque(InputPitch * PitchRate * Time.deltaTime, InputYaw * YawRate * Time.deltaTime, InputRoll * RollRate * Time.deltaTime, ForceMode.Force);
+
+        rigi.AddRelativeForce(new Vector3(0, 0, BaseLinearForce * SpeedZ*InputAcc*Time.deltaTime),ForceMode.Force);
 
         Debug.Log(rigi.velocity.sqrMagnitude);
-        Debug.DrawRay(transform.position, transform.forward*10);
+        Debug.DrawRay(transform.position, transform.forward * 10);
     }
 
-    void GetInput(ref float InputX,ref float InputZ)
+    void GetInput(ref float InputPitch, ref float InputRoll, ref float InputYaw, ref float InputAcc)
     {
-        InputX = Input.GetAxis("Vertical");
-        InputZ = Input.GetAxis("Horizontal");
+        InputAcc = Input.GetAxis("Vertical");
+        InputYaw = Input.GetAxis("Horizontal");
+
+        Vector3 mousePos = Input.mousePosition;
+
+        InputPitch = (mousePos.y - (Screen.height * 0.5f)) / (Screen.height * 0.5f);
+        InputRoll = (mousePos.x - (Screen.width * 0.5f)) / (Screen.width * 0.5f);
+
+        InputPitch = -Mathf.Clamp(InputPitch, -1.0f, 1.0f);
+        InputRoll = Mathf.Clamp(InputRoll, -1.0f, 1.0f);
     }
 
     private void OnCollisionEnter(Collision collision)
